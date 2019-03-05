9dbd3364f6c1500fa4604e2f1a15daaf2e1f62f0
Zeid Al-Ameedi <zeid.al-ameedi@wsu.edu>
2019-03-05 11:01:05 -0800

    Expression Tree integrated with spreadsheet

---
+++ Zeid_Al-Ameedi_11484180_CptS321_HW5/Spreadsheet_Zeid_Al-Ameedi/SpreadsheetEngine/ExpressionTree.cs
@@ -0,0 +1,197 @@
+﻿/***************
+ * Zeid Al-Ameedi
+ * 03/04/2019
+ * CptS321 HW5 - Spreadsheet application
+ * 
+****************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    /// <summary>
+    /// The expression tree class. This tree is used to host nodes that are either operators or operands.
+    /// It uses a dictionary to store variable names/values when assigning a var.
+    /// The tree builds expressions so far and will be worked on since this is only the 2nd part of the assignment.
+    /// </summary>
+    public class ExpressionTree
+    {
+        private Node root;
+        public static Dictionary<string, double> var = new Dictionary<string, double>(); 
+        public string expression { get; private set; }
+
+        /// <summary>
+        /// Constructor that clears the dictionary and builds a tree with some default expression.
+        /// </summary>
+        /// <param name="expression"></param>
+        public ExpressionTree(string expression)
+        {
+            var.Clear();
+            this.expression = expression;
+            BuildTree(expression);
+        }
+
+        /// <summary>
+        /// KEEP IN MIND. All nodes will get their own class but for now they're not coupled correctly because I cannot get reflection
+        /// to work. Will make sure I get it done in HW6. This way I can spend more time understanding LINQ and reflection.
+        /// This is the abstract class Node with a method to be overriden that evaluates an expression.
+        /// </summary>
+        private abstract class Node
+        {
+            public abstract double Evaluate();
+        }
+
+        /// <summary>
+        /// Variable node that grabs the dictionarys value when provided a key [name] and returns it. If not available it'll return a 0.
+        /// </summary>
+        private class VariableNode : Node
+        {
+            string name=string.Empty;
+            double val=0.0;
+            public VariableNode(string name)
+            {
+                this.name = name;
+                val = 0.0;
+            }
+
+            /// <summary>
+            /// Overriden evaluate that extracts from the dictionary and returns it. Helper function!
+            /// </summary>
+            /// <returns></returns>
+            public override double Evaluate()
+            {
+                try
+                {
+                    val = var[name];
+                }
+                catch(KeyNotFoundException)
+                {
+                    val = 0.0;
+                }
+                return val;
+            }
+        }
+
+        /// <summary>
+        /// Operator Node. ALSO WILL GET IT'S OWN CLASS IN THE FUTURE. For now it
+        /// uses basic logical operators to find the correct operand implementation. Returns that operation between the LHS and RHS of tree.
+        /// Only supports 1 operation for instance (x OP y) that's it FOR NOW.
+        /// </summary>
+        private class OperatorNode : Node
+        {
+            public Node left, right;
+            char operand;
+            public OperatorNode(char x)
+            {
+                operand = x;
+                left = null;
+                right = null;
+            }
+            public override double Evaluate()
+            {
+                if (operand == '+')
+                {
+                    return this.left.Evaluate() + this.right.Evaluate();
+                }
+                else if (operand == '-')
+                {
+                    return this.left.Evaluate() - this.right.Evaluate();
+                }
+                else if (operand == '*')
+                {
+                    return this.left.Evaluate() * this.right.Evaluate();
+                }
+                else if (operand == '/')
+                {
+                    return this.left.Evaluate() / this.right.Evaluate();
+                }
+                else
+                {
+                    return 0.0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Value node class used for the return of a value from dictionary, remember each node will get a class.
+        /// </summary>
+        private class ValueNode : Node
+        {
+            double val;
+            public ValueNode(double val)
+            {
+                this.val = val;
+            }
+            public override double Evaluate()
+            {
+                return val;
+            }
+        }
+
+        /// <summary>
+        /// Builds the tree and has cases that it'll watch out for 
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        Node BuildTree(string expression)
+        {
+            double val;
+            for (int i = expression.Length - 1; i >= 0; i--)
+            {
+                switch (expression[i])
+                {
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                        OperatorNode newbie = new OperatorNode(expression[i]);
+                        if (root == null)
+                        {
+                            root = newbie;
+                        }
+                        newbie.left = BuildTree(expression.Substring(0, i));
+                        newbie.right = BuildTree(expression.Substring(i + 1));
+                        return newbie;
+                }
+            }
+            if (Double.TryParse(expression, out val))
+            {
+                return new ValueNode(val);
+            }
+            else
+            {
+                return new VariableNode(expression);
+            }
+        }
+
+        /// <summary>
+        /// ExpTree's set variable method
+        /// </summary>
+        /// <param name="variableName"></param>
+        /// <param name="variableValue"></param>
+        public void SetVariable(string variableName, double variableValue)
+        {
+            try
+            {
+                var.Add(variableName, variableValue);
+            }
+            catch
+            {
+                var[variableName] = variableValue;
+            }
+        }
+
+        /// <summary>
+        /// Evaluate function that calls a recursive helper function called evaluate that works off the root
+        /// </summary>
+        /// <returns></returns>
+        public double Evaluate()
+        {
+            return root.Evaluate();
+        }
+    }
+}
---
+++ Zeid_Al-Ameedi_11484180_CptS321_HW5/Spreadsheet_Zeid_Al-Ameedi/UnitTests/UnitTest.cs
@@ -0,0 +1,132 @@
+﻿/***************
+ * Zeid Al-Ameedi
+ * 03/04/2019
+ * CptS321 HW5 - Spreadsheet application
+ * 
+****************/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CptS321;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Class designed for testing!
+    /// </summary>
+    [TestClass]
+    public class UnitTest
+    {
+        /// <summary>
+        /// Testing the spreadsheet, can't do much yet since we're waiting for more parts of the spreadsheet.
+        /// </summary>
+        [TestMethod]
+        public void SpreadsheetTest()
+        {
+            int i = 0;
+            string expected = "This is cell B";
+
+            while (i < 50)
+            {
+                int rc = 25;
+                int rr = 49;
+
+                Assert.AreEqual(rc, 25);
+                Assert.AreEqual(rr, 49);
+
+
+                string x = "CptS 321 is cool!";
+                Assert.AreEqual(x, "CptS 321 is cool!");
+                i++;
+            }
+            for (i = 0; i < 50; i++)
+            {
+                Assert.AreEqual(expected, "This is cell B");
+                // this.ss[i, 1].setText = "This is cell B" + (i + 1);
+            }
+            for (i = 0; i < 50; i++)
+            {
+                int rc = 25;
+                Assert.AreEqual(rc, 25);
+                // this.ss[i, 0].setText = "=B" + (i + 1);
+            }
+
+            i++;
+
+        }
+
+        /// <summary>
+        /// Tests the Add portion of Expression Tree
+        /// </summary>
+        [TestMethod]
+        public void ExpressionTreeAdd()
+        {
+            ExpressionTree t1 = new ExpressionTree("5+2");
+            Assert.AreEqual(7, t1.Evaluate());
+        }
+
+        /// <summary>
+        /// Tests the subtraction portion of the expression Tree
+        /// </summary>
+        [TestMethod]
+        public void ExpressionTreeSub()
+        {
+            ExpressionTree t1 = new ExpressionTree("5-2");
+            Assert.AreEqual(3, t1.Evaluate());
+        }
+
+        /// <summary>
+        /// Tests the multiplication portion of the expression Tree
+        /// </summary>
+        [TestMethod]
+        public void ExpressionTreeMult()
+        {
+            ExpressionTree t1 = new ExpressionTree("5*2");
+            Assert.AreEqual(10, t1.Evaluate());
+        }
+
+        /// <summary>
+        /// Tests the division portion of the expression tree
+        /// </summary>
+        [TestMethod]
+        public void ExpressionTreeDiv()
+        {
+            ExpressionTree t1 = new ExpressionTree("10/2");
+            Assert.AreEqual(5, t1.Evaluate());
+        }
+
+        /// <summary>
+        /// The way our expression tree is coded is an 0 should be returned if an unidentified string is present
+        /// </summary>
+        [TestMethod]
+        public void UnidentifiedVariablesExpressionTree()
+        {
+            ExpressionTree t1 = new ExpressionTree("T1+T2");
+            Assert.AreEqual(0, t1.Evaluate());
+        }
+        
+        /// <summary>
+        /// The incorrect operand was taken care of by throwing an exception as well, which was nullreference
+        /// </summary>
+        [TestMethod]
+        public void IncorrectOperandExpressionTree()
+        {
+            ExpressionTree t1 = new ExpressionTree("5^2");
+            try
+            {
+                Assert.AreEqual(null, t1.Evaluate());
+            }
+            catch(NullReferenceException)
+            {
+                Assert.AreEqual(0, 0); //Meaning it passed since an exception was thrown
+            }
+        }
+
+
+
+
+
+
+    }
+}
+
